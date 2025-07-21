bb33a7901949106df5522599494533f45474a12d
今井啓太 <imaikeita@imaikeitanoMacBook-Air.local>
2025-07-22 01:28:54 +0900

    Update header of works page

--- app/components/header/HeaderB.tsx
+++ app/components/header/HeaderB.tsx
@@ -32,23 +32,26 @@ export default function HeaderWithTabs({
   }, []);
 
   return (
-    <header className="bg-black text-white px-4 pt-5 pb-2 sticky top-0 z-50 border-b">
-      <div className="flex flex-col gap-2 md:flex-row md:items-end md:justify-between">
-        
-        {/* 左：タイトルと戻るボタン */}
-        <div className="flex items-center gap-2">
-          <Link href="/works" className="text-sm hover:text-gray-300">◀︎</Link>
-          <h1 className="text-2xl font-bold">{title}</h1>
-        </div>
+    <header className="bg-black text-white px-4 pt-5 pb-3 sticky top-0 z-50 border-b">
+      <div className="flex flex-row justify-between items-center">
 
-        {/* 中央：制作期間とスキル */}
-        <div className="flex flex-wrap gap-x-4 text-sm text-gray-400">
-          <span>project duration: {period}</span>
-          <span>tools: {tools.join(' / ')}</span>
+        {/* 左：タイトル・期間・ツール */}
+        <div className="flex gap-1 flex-row items-center gap-4">
+          <Link href="/works" className="text-m hover:text-gray-300 ml-3">◀︎</Link>
+          <div className="flex flex-col">
+            <div className="flex items-center gap-2 pl-1">
+              <h1 className="text-2xl font-bold">{title}</h1>
+            </div>
+            <div className="text-sm text-gray-400 mt-1 pl-1">
+              <span>Period: {period}</span>
+              <span className='mr-2 ml-2'> / </span>
+              <span>Tools: {tools.join(' / ')}</span>
+            </div>
+          </div>
         </div>
 
         {/* 右：タブ */}
-        <div className="mt-2 md:mt-0">
+        <div className="items-center">
           {isMobile ? (
             <select
               value={selectedTab}
@@ -67,7 +70,7 @@ export default function HeaderWithTabs({
                 <button
                   key={tab.id}
                   onClick={() => onChangeTab(tab.id)}
-                  className={`text-sm px-3 py-1 border rounded ${
+                  className={`text-sm px-4 py-1 border rounded ${
                     selectedTab === tab.id
                       ? 'bg-white text-black'
                       : 'text-white border-white hover:bg-gray-800'
@@ -79,6 +82,7 @@ export default function HeaderWithTabs({
             </nav>
           )}
         </div>
+
       </div>
     </header>
 
